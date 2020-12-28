78b7e0cc55a2a6dd1dfa472da3dcc699ec2ba87e
dyj <duanyj95@outlook.com>
2020-12-28 21:28:15 +0800

    initialize ubuntu branch

---
+++ zsh/alias.zsh
@@ -0,0 +1,26 @@
+alias ca='conda activate'
+alias ci='conda install'
+alias cs='conda search'
+alias cl='conda list'
+alias cda='conda deactivate'
+
+alias tb='tensorboard --logdir'
+
+alias tn='tmux new -s'
+alias ta='tmux a -t'
+alias tk='tmux kill-session -t'
+alias tl='tmux ls'
+
+alias ss='screen -S'
+alias sr='screen -R'
+alias sd='screen -d'
+alias sl='screen -ls'
+alias sc='echo $STY'
+
+alias ll='ls -alF'
+alias la='ls -a'
+
+alias ..='cd ..'
+alias ...='cd ../..'
+alias e='exit'
+alias cls='clear'
