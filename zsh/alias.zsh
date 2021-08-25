868785725a56a71327995242426896550bd25a1d
dyj <duanyj95@outlook.com>
2021-08-25 17:05:04 +0800

    add proxy

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -7,7 +7,14 @@ alias ml='mamba list'
 alias ca='conda activate'
 alias cda='conda deactivate'
 
-alias tb='tensorboard --logdir'
+if [[ "$OSTYPE" =~ ^darwin ]]; then
+    alias vnc16='autossh -M 20016 -fNL 30016:192.168.2.16:5993 jumper624'
+    alias vnc23='autossh -M 20023 -fNL 30023:192.168.2.23:3389 jumper624'
+    alias vnc27='autossh -M 20027 -fNL 30027:192.168.1.101:5993 jumper323'
+    alias vnc33='autossh -M 20033 -fNL 30033:192.168.1.106:5937 jumper323'
+elif [[ "$OSTYPE" =~ ^linux ]]; then
+    alias tb='tensorboard --logdir'
+fi
 
 alias tn='tmux new -s'
 alias ta='tmux a -t'
