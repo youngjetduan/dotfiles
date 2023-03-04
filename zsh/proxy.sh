83f07265e834bb8be55db761bcea043e93e2837f
dyj <duanyj95@outlook.com>
2023-03-04 16:09:11 +0800

    add git_mirror

--- zsh/proxy.sh
+++ zsh/proxy.sh
@@ -1,2 +1,7 @@
-alias proxy_on="export all_proxy=socks5://127.0.0.1:7890"
-alias proxy_off="unset all_proxy"
+if [[ "$OSTYPE" =~ ^darwin ]]; then
+    alias proxy_on="export all_proxy=socks5://127.0.0.1:7890"
+    alias proxy_off="unset all_proxy"
+elif [[ "$OSTYPE" =~ ^linux ]]; then
+    alias git_mirror='git config --global url."https://gitclone.com/github.com".insteadOf https://github.com'
+    alias git_unset='git config --global --unset url.https://gitclone.com/github.com.insteadof'
+fi
