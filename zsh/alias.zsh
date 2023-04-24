26c033363fb831372ae3f139e85bfeacc1da8b41
dyj <duanyj95@outlook.com>
2023-04-24 14:03:58 +0800

    fix tmux clear and merge proxy to alias file

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,3 +1,5 @@
+# conda
+echo "set alias for conda command"
 alias ci='conda install'
 alias cs='conda search'
 alias cl='conda list'
@@ -7,11 +9,15 @@ alias ml='mamba list'
 alias ca='conda activate'
 alias cda='conda deactivate'
 
+# pyenv
+echo "set alias for pyenv command"
 alias pyvv='pyenv virtualenv'
 alias pyvs='pyenv virtualenvs'
 alias pya='pyenv activate'
 alias pyd='pyenv deactivate'
 
+# vnc
+echo "set alias for vnc command"
 if [[ "$OSTYPE" =~ ^darwin ]]; then
     alias vnc16='autossh -M 20016 -fNL 30016:192.168.2.16:5993 jumper624'
     alias vnc23='autossh -M 20023 -fNL 30023:192.168.2.23:3389 jumper624'
@@ -24,23 +30,36 @@ elif [[ "$OSTYPE" =~ ^linux ]]; then
     alias tb='tensorboard --logdir'
 fi
 
+# tmux
+echo "set alias for tmux command"
 alias tn='tmux new -s'
 alias ta='tmux a -t'
 alias tk='tmux kill-session -t'
 alias tl='tmux ls'
 
+# linux command
+echo "set alias for linux system command"
 alias ll='ls -alF'
 alias la='ls -a'
-
 alias ..='cd ..'
 alias ...='cd ../..'
 alias e='exit'
 alias cls=clear_history
 
-
 clear_history(){
     clear
-    if [ "$TERM_PROGRAM" = "tmux" ]; then
+    if [[ -n "$TMUX" ]]; then
         tmux clear-history
     fi
 }
+
+# proxy
+echo "set alias for proxy command"
+alias git_mirror='git config --global url."https://gitclone.com/github.com".insteadOf https://github.com'
+alias git_unset='git config --global --unset url.https://gitclone.com/github.com.insteadof'
+
+if [[ "$OSTYPE" =~ ^darwin ]]; then
+    echo "is mac"
+    alias proxy_on='export all_proxy=socks5://127.0.0.1:7890'
+    alias proxy_off='unset all_proxy'
+fi
--- zsh/proxy.sh
+++
@@ -1,7 +0,0 @@
-if [[ "$OSTYPE" =~ ^darwin ]]; then
-    alias proxy_on="export all_proxy=socks5://127.0.0.1:7890"
-    alias proxy_off="unset all_proxy"
-elif [[ "$OSTYPE" =~ ^linux ]]; then
-    alias git_mirror='git config --global url."https://gitclone.com/github.com".insteadOf https://github.com'
-    alias git_unset='git config --global --unset url.https://gitclone.com/github.com.insteadof'
-fi
